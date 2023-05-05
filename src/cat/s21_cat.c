319a495a3db82ed2dd0f965405c3bcb1fd395449
Runcel Kaye <runcelka@ev-f3.kzn.21-school.ru>
2023-05-05 17:37:08 +0300

    flag -t -e -n

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int file_position(int argc, char **argv);
-void read_all_files(int argc, char **argv, int file_post);
+void read_all_files(int argc, char **argv, int file_post, int *arr_flags);
 int search_all_flags(char **argv, int file_post, int *arr_flags);
 
 int main(int argc, char **argv) {
@@ -8,13 +8,12 @@ int main(int argc, char **argv) {
   int file_post = file_position(argc, argv);
   int flags = search_all_flags(argv, file_post, arr_flags);
   if (flags != 1) {
-    read_all_files(argc, argv, file_post);
-    for (int i = 1; i < 8; i++) {
+    read_all_files(argc, argv, file_post, arr_flags);
+    for (int i = 0; i < 8; i++) {
       printf("%d", arr_flags[i]);
     }
     printf("\n");
   }
-  
 
   return 0;
 }
@@ -27,14 +26,35 @@ int file_position(int argc, char **argv) {
   return i;
 }
 
-void read_all_files(int argc, char **argv, int file_post) {
+void read_all_files(int argc, char **argv, int file_post, int *arr_flags) {
   for (int i = file_post; i < argc; i++) {
     FILE *file = fopen(argv[i], "r");
+    int iter = 1;
+    int end_line = 0;
     if (file == NULL) {
-      printf("Error. File does not exist or can't be opened.\n");
+      printf("cat: %s: No such file or directory\n", argv[i]);
     } else {
       char content;
-      while ((content = fgetc(file)) != EOF) printf("%c", content);
+      while ((content = fgetc(file)) != EOF) {
+        if ((arr_flags[4] == 1) && (content == '\t')) /* flag -t*/ {
+          printf("^I");
+        } else if ((arr_flags[1] == 1) && (content == '\n')) /* flag -e */ {
+          printf("$\n");
+
+        } else if ((arr_flags[2] == 1) &&
+                   (end_line != 0 || iter == 1)) /* flag -n */ {
+          printf("%6d\t", iter);
+          printf("%c", content);
+          iter++;
+        } else {
+          printf("%c", content);
+        }
+        if (content == '\n') {
+          end_line++;
+        } else {
+          end_line = 0;
+        }
+      }
     }
     fclose(file);
   }
@@ -47,7 +67,7 @@ int search_all_flags(char **argv, int file_post, int *arr_flags) {
     for (int j = 1; argv[i][j] != '\0'; j++) {
       int success = 0;
       for (int k = 0; k < 8; k++) {
-        if (argv[i][j] == flags_amount[k]){
+        if (argv[i][j] == flags_amount[k]) {
           success = 1;
           arr_flags[k] = 1;
         }
@@ -56,12 +76,8 @@ int search_all_flags(char **argv, int file_post, int *arr_flags) {
         res = 1;
         printf("cat: illegal option -- %c\n", argv[i][j]);
       }
-    } 
+    }
   }
-
-
-
-
 
   return res;
 }
