07cc9d61f955da5f7003e65f29ca02131e5cd381
Runcel Kaye <runcelka@ev-f3.kzn.21-school.ru>
2023-05-14 15:16:29 +0300

    bug fix -s -v, gnu

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -12,9 +12,8 @@ int main(int argc, char **argv) {
   if (flags != 1) {
     read_all_files(argc, argv, file_post, arr_flags);
     for (int i = 0; i < 8; i++) {
-      printf("%d", arr_flags[i]);
+      //printf("%d", arr_flags[i]);
     }
-    printf("\n");
   }
 
   return 0;
@@ -22,7 +21,7 @@ int main(int argc, char **argv) {
 
 int file_position(int argc, char **argv) {
   int i;
-  for (i = 1; i < argc, argv[i][0] == '-'; i++)
+  for (i = 1; i < argc && argv[i][0] == '-'; i++)
     ;
 
   return i;
@@ -32,12 +31,19 @@ void read_all_files(int argc, char **argv, int file_post, int *arr_flags) {
   for (int i = file_post; i < argc; i++) {
     FILE *file = fopen(argv[i], "r");
     int iter = 1;
-    int end_line = 0;
+    int end_line = 1;
+    int first_end_line = -1;
+    int position_char = 0;
     if (file == NULL) {
-      printf("cat: %s: No such file or directory\n", argv[i]);
+      fprintf(stderr, "cat: %s: No such file or directory\n", argv[i]);
     } else {
       char current_content;
       while ((current_content = fgetc(file)) != EOF) {
+        // printf("<%d>", end_line);
+        if ((arr_flags[3] == 1) && end_line > 1 && current_content == '\n') {
+          continue;
+        }
+
         if ((arr_flags[0] == 1) && (current_content != '\n') &&
             (end_line >= 1 || iter == 1)) { /* flag -b */
           printf("%6d\t", iter);
@@ -48,33 +54,35 @@ void read_all_files(int argc, char **argv, int file_post, int *arr_flags) {
           iter++;
         }
 
-        if ((arr_flags[4] == 1) && (current_content == '\t')) { /* flag -t*/
+        if ((arr_flags[4] == 1 || arr_flags[7] == 1) && (current_content == '\t')) { /* flag -t, T*/
           printf("^I");
-        } else if ((arr_flags[7] == 1) &&
-                   (current_content == '\t')) { /* flag -T*/
-          printf("^I");
-        } else if ((arr_flags[3] == 1) &&
-                   (end_line >= 1 && current_content == '\n')) {
-          /* flag -s */  // was >= 2
-        } else if ((arr_flags[1] == 1) &&
-                   (current_content == '\n')) { /* flag -e */
+        }
+        // else if ((arr_flags[3] == 1) &&
+        //            ((end_line >= 2 && current_content == '\n')|| (end_line == 1 && current_content == '\n' && first_end_line == 0))) {
+        //             //  continue;
+        //   /* flag -s */ 
+        // }
+        else if ((arr_flags[1] == 1 || arr_flags[6] == 1) &&
+                   (current_content == '\n')) { /* flag -e, E */
           printf("$\n");
-        } else if ((arr_flags[6] == 1) &&
-                   (current_content == '\n')) { /* flag -E */
-          printf("$\n");
-        if (arr_flags[5] ==1 || arr_flags[1] == 1 || arr_flags[4] == 1) {
-          if (current_content >= 0 && current_content < 32 && (current_content != 9) && (current_content != 10)) {
-            printf("^%c", current_content += 64);
+        } else if (arr_flags[5] == 1 || arr_flags[1] == 1 || arr_flags[4] == 1) {
+          if (current_content >= 0 && current_content < 32 &&
+              (current_content != 9) && (current_content != 10)) {
+            printf("^%c", current_content + 64);
           }
-          if (current_content == 127) { printf("^?"); 
+          if (current_content == 127) {
+            printf("^?");
           }
-          if (current_content >= 32 && current_content < 127) {
+          if ((current_content >= 32 && current_content < 127) ||
+              current_content == 9 || current_content == 10) {
             printf("%c", current_content);
           }
           if (current_content < 0) {
-            current_content &= 127; // бинарное (побитовое) умножение, отбрасывающее отрицательный знак в первом бите
+            current_content &=
+                127;  // бинарное (побитовое) умножение, отбрасывающее
+                      // отрицательный знак в первом бите
             int new_current_content = current_content + 128;
-            
+
             if (new_current_content >= 128 && new_current_content < 160) {
               printf("M-^%c", new_current_content - 64);
             }
@@ -84,19 +92,24 @@ void read_all_files(int argc, char **argv, int file_post, int *arr_flags) {
             if (new_current_content >= 160 && new_current_content < 255) {
               printf("%c", new_current_content);
             }
-
           }
-        }
-  
-        } else {
+
+        }  else {
           printf("%c", current_content);
         }
 
         if (current_content == '\n') {
           end_line++;
+          if (first_end_line == -1) {
+            first_end_line = position_char;
+          }
+          position_char = -1;
         } else {
+          first_end_line = -1;
           end_line = 0;
         }
+        position_char++;
+        
       }
     }
     fclose(file);
@@ -105,7 +118,49 @@ void read_all_files(int argc, char **argv, int file_post, int *arr_flags) {
 
 int search_all_flags(char **argv, int file_post, int *arr_flags) {
   int res = 0;
-  char linux_flags[][17] = {"--number-nonblank", "--number", "--squeeze-blank"};
+  //пример построчного считывания массива
+  
+  char linux_flags[][17] = {"-b", "-e", "-n", "-s", "-t", "-v", "-E", "-T" ,"--number-nonblank", "--number", "--squeeze-blank"};
+  char match_gnu_flags[][] = {'b', 'e', 'n', 's', 't', 'v', 'E', 'T', 'b', 'n', 's'};
+
+
+  цикл i = 0; i < file_post
+    цикл по j = 0; j < длина(linux_flags)(6)
+      если строка argv[i] совпадает со строкой linux_flags[j] и j > 2
+        переменная = match_gnu_flags[i];
+        return match_gnu_flags[i];
+      иначе это гну флаг и:
+        переменная = match_gnu_flags[i];
+        return match_gnu_flags[i];
+  return переменная
+
+
+
+
+////////////////////////////////// посимвольно
+здесь тоже есть какой-то цикл для итерации по аргв
+    если strlen(argv) > 1 && АРГВ[k][1] == '-'
+      парсить флаг гну
+      если strlen(argv) > 2
+        запускаем цикл для итерации по элементам "linux_flags" 
+        цикл от i = 0 до i < 3
+          {
+            цикл от j = 2 до j < strlen(argv) {
+              если linux_flags[i][j] == arvg[k][j]
+            }
+            match_gnu_flags[i] активируешь флаг
+
+          return match_gnu_flags[i]
+
+            
+          }
+      {
+        цикл от i = 2 до  i < strlen(argv)
+
+      }
+    иначе //реализация обычных флагов, если сделать все в одну можно убрать вторую часть
+
+
   char flags_amount[8] = {'b', 'e', 'n', 's', 't', 'v', 'E', 'T'};
   for (int i = 1; i < file_post; i++) {
     for (int j = 1; argv[i][j] != '\0'; j++) {
@@ -118,7 +173,7 @@ int search_all_flags(char **argv, int file_post, int *arr_flags) {
       }
       if (success == 0) {
         res = 1;
-        printf("cat: illegal option -- %c\n", argv[i][j]);
+        fprintf(stderr, "cat: illegal option -- %c\n", argv[i][j]);
       }
     }
 
@@ -131,4 +186,3 @@ int search_all_flags(char **argv, int file_post, int *arr_flags) {
 
   return res;
 }
-
