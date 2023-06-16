1232581a803bdd872cd68f1733c4d0f4a64cf682
Runcel Kaye <runcelka@ev-d3.kzn.21-school.ru>
2023-06-16 20:17:36 +0300

    fixing bugs, filling files array

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -1,27 +1,31 @@
 #include "s21_grep.h"
-void files_samples(int argc, char **argv, char arr_templates[1000][1000],
-                   int *counter);
-void parsing(int argc, char **argv, int *counter);
+void files_samples(int argc, char **argv, char arr_templates[100][100],
+                   int *counter_templates, int *counter_files);
+void parsing(int argc, char **argv, int *counter_templates, int *counter_files);
 
-int file_counter(int argc, char **argv);
+int file_counter_templates(int argc, char **argv);
 
-int template_find(char arr_flags[10], char **arr_templates, char **arr_files,
-                  char *arr_line_buffer);
+int template_find(char arr_flags[10], char **arr_templates,
+                  char *arr_line_buffer, int *counter_templates);
+int file_opening(char arr_flags[10], char **arr_templates, char **arr_files, int *counter_templates, int *counter_files);
 
 int main(int argc, char **argv) {
-  int counter = 0;
+  int counter_templates = 0;
+  int counter_files = 0;
   char arr_flags[10], **arr_templates, **arr_files;
 
-  parsing(argc, argv, &counter);
-  file_opening(arr_flags, arr_templates, arr_files);
+  parsing(argc, argv, &counter_templates, &counter_files);
+
+  
+  file_opening(arr_flags, arr_templates, arr_files, &counter_templates, &counter_files);
   return 0;
 }
 
-void parsing(int argc, char **argv, int *counter) {
-  int flag_options, i = 0, n = 0;
+void parsing(int argc, char **argv, int *counter_templates, int *counter_files) {
+  int flag_options, i = 0;
   char arr_flags[10] = {0};
-  char arr_templates[1000][1000] = {0};
-  char arr_files[1000][1000] = {0};
+  char arr_templates[100][100] = {0}; //не нужно создавать в парсинге а нужно из мейна передать в функцию, лучше всего создать структуры куда запихать все массивы и везде таскать (создать в заголовке)
+  char arr_files[100][100] = {0};
   while ((flag_options = getopt_long(argc, argv, "e:ivclnhsf:o", NULL, NULL)) !=
          -1) {
     if (strchr(arr_flags, flag_options) == 0 && (flag_options != '?')) {
@@ -29,29 +33,35 @@ void parsing(int argc, char **argv, int *counter) {
       i++;
     }
     if (flag_options == 'e') {
-      strcpy(arr_templates[*counter], optarg);
-      (*counter)++;
+      strcpy(arr_templates[*counter_templates], optarg);
+      (*counter_templates)++;
     } else if (flag_options == 'f') {
-      files_samples(argc, argv, arr_templates, counter);
+      files_samples(argc, argv, arr_templates, counter_templates, counter_files);
     }
   }
   if ((strchr(arr_flags, 'e') == 0) && (strchr(arr_flags, 'f')) == 0) {
     strcpy(arr_templates[0], argv[optind]);
     printf("%d\n", optind);
-    strcpy(arr_files[0], argv[optind + 1]);
-
+    for (*counter_files = 0; ((*counter_files) < argc) && (optind + 1 < argc); ((*counter_files)++) , (optind++)) {
+      strcpy(arr_files[*counter_files], argv[optind+1]);
+    
+    }
   } else {
-    for (int k = 0; k < argc && optind < argc; k++ && optind++) {
-      strcpy(arr_files[k], argv[optind]);
+    for (*counter_files = 0; ((*counter_files) < argc) && (optind < argc); ((*counter_files)++) , (optind++)) {
+      strcpy(arr_files[*counter_files], argv[optind]);
     }
   }
   printf("%s\n", arr_flags);
-  printf("%s\n", arr_templates[0]);
-  printf("%s\n", arr_files[0]);
+  for (int m = 0; m <= *counter_templates; m++) {
+  printf("%s\n", arr_templates[m]);
+  }
+  printf("files:\n");
+  for (int l = 0; l < *counter_files; l++)
+  printf("%s\n", arr_files[l]);
 }
 
 void files_samples(int argc, char **argv, char arr_templates[1000][1000],
-                   int *counter) {
+                   int *counter_templates, int *counter_files) {
   size_t n = 0;
   char *arr_line_buffer = {0};
   FILE *file = fopen(optarg, "r");
@@ -65,19 +75,22 @@ void files_samples(int argc, char **argv, char arr_templates[1000][1000],
         *line_pos = '\0';
       }
 
-      strcpy(arr_templates[*counter], arr_line_buffer);
+      strcpy(arr_templates[*counter_templates], arr_line_buffer);
 
-      (*counter)++;
+      (*counter_templates)++;
     }
     free(arr_line_buffer);
   }
 }
-int template_find(char arr_flags[10], char **arr_templates, char **arr_files,
-                  char *arr_line_buffer) {
+int template_find(char arr_flags[10], char **arr_templates,
+                  char *arr_line_buffer, int *counter_templates) {
   regex_t regex;
-  regcomp(&regex, arr_templates, 0);
-  if (regexec(&regex, arr_line_buffer, 0, NULL, 0) == 0) {
-    printf("Yes!"); //прописать вывод всего 
+  for (int i = 0; i < (*counter_templates); i++) {
+    regcomp(&regex, arr_templates[i], 0);
+    if (regexec(&regex, arr_line_buffer, 0, NULL, 0) == 0) {
+      printf("Yes!"); //прописать вывод всего 
+      //flags_content(flag_options);
+    }
   }
   regfree(&regex);
   return 0;
@@ -114,18 +127,20 @@ int template_find(char arr_flags[10], char **arr_templates, char **arr_files,
 //     }
 // }
 
-int file_opening(char arr_flags[10], char **arr_templates, char **arr_files) {
+int file_opening(char arr_flags[10], char **arr_templates, char **arr_files, int *counter_templates, int *counter_files) {
   size_t n = 0;
   char *arr_line_buffer = {0};
-
-  FILE *file = fopen(**arr_files, "r");
+  for (int i = 0; i < *counter_files; i++) {
+  FILE *file =fopen(arr_files[i], "r");
   if (file == NULL) {
-    fprintf(stderr, "grep: %s: No such file or directory\n", **arr_files);
+    fprintf(stderr, "grep: %s: No such file or directory\n", *arr_files);
   } else {
     char current_line;
     while ((current_line = getline(&arr_line_buffer, &n, file)) != EOF) {
-      template_find(arr_flags, arr_templates, arr_files, arr_line_buffer);
+      template_find(arr_flags, arr_templates, arr_line_buffer, counter_templates);
+     
     }
   }
   fclose(file);
+  }
 }
